5d7b6b9c05d42178b3bd6f0171cbc2e0ace6ab1c
Alee985 <49112854+Alee985@users.noreply.github.com>
2021-09-03 11:59:34 +0500

    Component display Products

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import axios from 'axios';
+import {useParams} from 'react-router-dom'
 const ProductDetail = () => {
 
+    const {pid}=useParams();
+    console.log(pid);
+    
     return(
         <div>
             <h2>Product Listing</h2>
